259ecbdd1a3e81168c7e7b0b32070a5c59f38eee
bhagyashribadgyujar <me.bhagyashribadgujar@gmail.com>
2019-03-29 13:03:57 +0530

    validation

---
+++ FileUploadValidation/Controllers/FileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileUploadValidation.Models;
+using System.Web.UI.WebControls;
+
+
+namespace FileUploadValidation.Controllers
+{
+    public class FileController : Controller
+    {
+        //  
+        // GET: /File/  
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult fileupload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult fileupload(HttpPostedFileBase file)
+        {
+            FileUpload1 fs = new FileUpload1();
+            fs.filesize = 550;
+            string us = fs.UploadUserFile(file);
+            if (us != null)
+            {
+                ViewBag.ResultErrorMessage = fs.ErrorMessage;
+            }
+            return View();
+        }
+    }
+}
---
+++ FileUploadValidation/Models/FileUpload1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+namespace FileUploadValidation.Models
+{
+    public class FileUpload1
+    {
+        public string ErrorMessage { get; set; }
+        public decimal filesize { get; set; }
+        public string UploadUserFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                var supportedTypes = new[] { "txt", "doc", "docx", "pdf", "xls", "xlsx" };
+                var fileExt = System.IO.Path.GetExtension(file.FileName).Substring(1);
+                if (!supportedTypes.Contains(fileExt))
+                {
+                    ErrorMessage = "File Extension Is InValid - Only Upload WORD/PDF/EXCEL/TXT File";
+                    return ErrorMessage;
+                }
+                else if (file.ContentLength > (filesize * 1024))
+                {
+                    ErrorMessage = "File size Should Be UpTo " + filesize + "KB";
+                    return ErrorMessage;
+                }
+                else
+                {
+                    ErrorMessage = "File Is Successfully Uploaded";
+                    return ErrorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = "Upload Container Should Not Be Empty or Contact Admin";
+                return ErrorMessage;
+            }
+        }
+    }
+}
